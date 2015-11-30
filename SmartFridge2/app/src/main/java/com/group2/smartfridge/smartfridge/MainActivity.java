a4ecf3703275e809de136f7e50e475c0e3aaf7c1
aplanchamp <aplanchamp@gmail.com>
2015-11-30 14:56:27 +0100

    suppression fragments inutiles

--- SmartFridge2/app/src/main/java/com/group2/smartfridge/smartfridge/MainActivity.java
+++ SmartFridge2/app/src/main/java/com/group2/smartfridge/smartfridge/MainActivity.java
@@ -1,9 +1,7 @@
 package com.group2.smartfridge.smartfridge;
 
-import android.app.Dialog;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,12 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.group2.smartfridge.smartfridge.database.DatabaseTestActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -95,27 +87,27 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_floor2) {
 
-            fragment = new FragmentFloor2();
+            fragment = new FragmentFloor();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.mainFrame, fragment);
             ft.commit();
 
         } else if (id == R.id.nav_floor1) {
 
-            fragment = new FragmentFloor1();
+            fragment = new FragmentFloor();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.mainFrame, fragment);
             ft.commit();
 
         } else if (id == R.id.nav_vegetable) {
 
-            fragment = new FragmentVegetable();
+            fragment = new FragmentFloor();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.mainFrame, fragment);
             ft.commit();
 
         }else if (id == R.id.nav_door) {
-            fragment = new FragmentDoor();
+            fragment = new FragmentFloor();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.mainFrame, fragment);
             ft.commit();
